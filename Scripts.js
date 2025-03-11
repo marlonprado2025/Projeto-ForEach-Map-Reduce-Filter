eec0ed24fce47d15cb00a5d136aca7951c5761c2
marlonprado2025 <marlon123po2@gmail.com>
2025-03-11 00:30:19 -0300

    Update commit

--- Scripts.js
+++ Scripts.js
@@ -1,37 +1,68 @@
 
 const lista = document.querySelector('ul')
 const showAllButtun = document.querySelector('.show-All')
-const mapAllButtun =document.querySelector('.map-all')
+const mapAll = document.querySelector('.mapAll')
+const sumAllTotal = document.querySelector('.sumAll')
+const filterVegan = document.querySelector('.filterVegan')
 
+let myLi = ''
 
-function showAll() {
- 
- let myLi =''
-    menuOptions.forEach((produto) => {
+function FshowAll(ArrayNew) {
+        myLi = ''    //Para zerar antes de colocala como abaixo com algo, para nao gerar duplicação, Ou posso colocar a (let myLi = '') aqui dentro.
 
-        myLi +=
-    `
+        ArrayNew.forEach((produt) => {
+
+                myLi +=
+                        `
             <li>
-                    <img src=${produto.src}>
-                    <p>${produto.name}</p>
-                    <p class="price"> R$${produto.price}</p>
-            </li>
-            
-    `  
-    })
-    
-    lista.innerHTML = myLi
+                <img src=${produt.src}>
+                <p>${produt.name}</p>
+                <p class="price"> R$${produt.price.toFixed(2)}</p>
+            </li>            
+        `
+        })
+
+        lista.innerHTML = myLi
 }
 
+showAllButtun.addEventListener('click', () => FshowAll(menuOptions))
 
-function mapAllitem() {
-   const newPrice = menuOptions.map((price)=>({
-    name:price.name,
-    price: price.price*0.9,
-    src: price.src
-   }))
-   console.log(newPrice)
+function mapAllShow() {
+        const newPrice = menuOptions.map((product) => ({
+                name: product.name + ` ${'com 10% de desconto!'}`,
+                price: product.price * 0.9,
+                vegan: product.vegan,
+                src: product.src
+                //////////////////<!--Oou poderei usar o Spread Operator
+                // ...product, price: product.price * 0.9 -->
+        }
+        ))
+
+        FshowAll(newPrice)
 }
 
-showAllButtun.addEventListener('click', showAll)
-mapAllButtun.addEventListener('click',mapAllitem)
+mapAll.addEventListener('click', mapAllShow)
+
+function sumAll() {
+        const valueTotal = menuOptions.reduce((acc, curr) => {
+
+                return acc + curr.price
+        }, 0)
+        lista.innerHTML = `
+                      <li>
+                           <p>VALOR TOTAL : R$${valueTotal.toFixed(2)}</p>
+                      </li>            
+                  `
+}
+
+sumAllTotal.addEventListener('click', sumAll)
+
+
+function filterV() {
+        const filterJustVegan = menuOptions.filter(filtro => {
+                if (filtro.vegan === true) { return true }
+        })
+        FshowAll(filterJustVegan)
+}
+
+filterVegan.addEventListener('click', filterV)

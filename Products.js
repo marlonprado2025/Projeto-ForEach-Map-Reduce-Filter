23995479b20d36ef12ce50ab7be2dec6f31325d0
MARLON PRADO <marlon123po2@gmail.com>
2025-03-12 23:46:19 -0300

    Update Products.js

--- Products.js
+++ Products.js
@@ -1,10 +1,10 @@
 //Lista de produtos
 const menuOptions = [
    
-    {        name: 'X - Bacon',         price: 34,        vegan: false,        src: "./img/xbacon.png"      },
-    {        name: 'Monstruoso',      price: 50,        vegan: false,        src: "./img/monstruoso.png"    },
-    {        name: 'X - Egg Bacon',     price: 39,        vegan: false,        src: "./img/x-bacon-egg.png" },  
-    {        name: 'Big Vegano',      price: 55,        vegan: true,         src: "./img/big-vegan.png"     },
-    {        name: 'X - Vegano',      price: 45,        vegan: true,         src: "./img/xvegan.png"        },
-    {        name: 'X - Salada',        price: 30,        vegan: false,        src: "./img/xsalada.jpeg"    },
+    {        name: 'X - Bacon',         price: 34,        vegan: false,        src: "./img/xbacon.png"        },
+    {        name: 'Monstruoso',      price: 50,        vegan: false,          src: "./img/monstruoso.png"    },
+    {        name: 'X - Egg Bacon',     price: 39,        vegan: false,        src: "./img/x-bacon-egg.png"   },  
+    {        name: 'Big Vegano',      price: 55,        vegan: true,           src: "./img/big-vegan.png"     },
+    {        name: 'X - Vegano',      price: 45,        vegan: true,           src: "./img/xvegan.png"        },
+    {        name: 'X - Salada',        price: 30,        vegan: false,        src: "./img/xsalada.jpeg"      },
 ]
